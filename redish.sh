59217aa7b3310126593416a7d93f5ddf65146313
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-31 17:11:16 +0530

    xcs

--- redish.sh
+++ redish.sh
@@ -9,6 +9,6 @@ echo -e "\e[33m Install redis\e[0m"
 yum install redis -y   &>>/tmp/roboshop.log
 sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf  /etc/redis/redis.conf
 
-echo -e "\e[33m Restart redis\e[0m"
+echo -e "\e[33m Install redis repo\e[0m"
 systemctl enable redis  &>>/tmp/roboshop.log
-systemctl start redis    &>>/tmp/roboshop.log
+systemctl restart redis    &>>/tmp/roboshop.log
