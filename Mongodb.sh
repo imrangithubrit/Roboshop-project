ae9da07e0af04929c381d7bbe7ae90fe0458b28f
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-31 12:43:23 +0530

    Uodated done

--- Mongodb.sh
+++ Mongodb.sh
@@ -5,6 +5,10 @@ cp Mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 echo -e "\e[33m Installing the mongodb\e[0m"
 yum install mongodb-org -y   &>>/tmp/roboshop.log
 
+echo -e "\e[33m Uodated mongodb.repo\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+
 echo -e "\e[33m Starting the mongodb\e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
 
